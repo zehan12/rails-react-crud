719a4d1c6eba99dca7bca2327656d3136affd3ac
Zehan Khan <zkhan@hootboard.com>
2024-04-04 10:10:00 +0530

    feat: :package: add mutiple packages

--- command.sh
+++ command.sh
@@ -28,4 +28,8 @@ mkdir -p ./app/javascript/packs/
 mv ./app/javascript/application.js ./app/javascript/packs/application.js
 
 # add browserlistrc
-echo "cover 95%" > .browserslistrc  
+echo "cover 95%" > .browserslistrc
+
+# add list of packages
+yarn add @svgr/webpack@^8.1.0 babel-plugin-dynamic-import-node@^2.3.3 babel-plugin-macros@^3.1.0 babel-plugin-js-logger@^1.0.17 css-loader@^6.8.1 dotenv-webpack@^8.0.1 i18next ignore-loader@^0.1.2 mini-css-extract-plugin@^2.7.6 js-logger@^1.6.1 postcss@^8.4.29 postcss-flexbugs-fixes@^5.0.2 postcss-import@^15.1.0 postcss-loader@^7.3.3 postcss-preset-env@^9.1.2 process@^0.11.10 ramda sass@^1.66.1 sass-loader@^13.3.2 source-map-loader@^4.0.1 style-loader@^3.3.3
+
