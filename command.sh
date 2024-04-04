276fa1adbf3839817bd89920bf8e36f922d1c478
Zehan Khan <zkhan@hootboard.com>
2024-04-04 10:08:07 +0530

    feat: :bento: update app folder and add browserslistrc file & remove from package.json

--- command.sh
+++ command.sh
@@ -22,3 +22,10 @@ yarn upgrade webpack-dev-server@4.13.1
 
 # config webpacker from wheel repo
 curl -o "config/webpacker.yml" "https://raw.githubusercontent.com/bigbinary/wheel/main/config/webpacker.yml"
+
+# update app folder structure
+mkdir -p ./app/javascript/packs/
+mv ./app/javascript/application.js ./app/javascript/packs/application.js
+
+# add browserlistrc
+echo "cover 95%" > .browserslistrc  
